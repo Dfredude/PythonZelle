7c50f8eb96cc460ac038a1fb07463aef26211c0c
Alfredo Dominguez <dominguezlucio@outlook.com>
2021-10-09 16:33:45 -0700

    Last pushs

---
+++ Chapter-12/6. Bridge.py
@@ -0,0 +1,9 @@
+#Simple bridge game, not casino games savvy, really bored me out so I skipped it.
+from graphics import *
+            
+            
+
+
+
+
+
--- Chapter-13/searching_algorithms.py
+++ Chapter-13/searching_algorithms.py
@@ -3,12 +3,14 @@ def main():
     print(linearSearch(2, [2,3,5,1,2,9]))
     print(binarySearch(2, [1,2,3,5,6,9]))
 
+#This is a linear time/n algorithm. Time required is linearly/proportional to the size 'n' of the sequence;
 def linearSearch(item, sequence:list):
     for i in range(len(sequence)):
         if sequence[i] == item:
             return i
     return -1
 
+#This is a log time algorithm, each iteration doubles the problem/sequence size
 def binarySearch(item, sorted_sequence:list):
     high = len(sorted_sequence) - 1
     low = 0
