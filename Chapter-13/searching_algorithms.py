58b41eaef3b9291784d58ff8514c39403241ebe1
Alfredo Dominguez <dominguezlucio@outlook.com>
2021-10-08 18:51:01 -0700

    Last Exercises

--- Chapter-13/searching_algorithms.py
+++ Chapter-13/searching_algorithms.py
@@ -1,12 +1,27 @@
 
 def main():
     print(linearSearch(2, [2,3,5,1,2,9]))
+    print(binarySearch(2, [1,2,3,5,6,9]))
 
 def linearSearch(item, sequence:list):
     for i in range(len(sequence)):
         if sequence[i] == item:
             return i
     return -1
+
+def binarySearch(item, sorted_sequence:list):
+    high = len(sorted_sequence) - 1
+    low = 0
+    mid = high//2
+    while low <= high:
+        if item > sorted_sequence[mid]: 
+            low = mid
+            mid = (mid+high)//2
+        elif item < sorted_sequence[mid]:
+            high = mid
+            mid = mid//2
+        elif item == sorted_sequence[mid]: return mid
+
 
 if __name__ == "__main__":
     main()
